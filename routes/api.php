77f7b6fdce1edc695771bf9759e53b512322fdc1
Daniel Tarus <tarusdaniel89@gmail.com>
2024-09-10 18:43:23 +0300

    sanitization

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/register', [AuthController::class, 'register']);
+//Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
@@ -21,8 +21,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('/tasks/{id}', [TasksController::class, 'update']);
     Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
 });
-//Route::get('/tasks', [TasksController::class, 'index']);
-//Route::get('/tasks/{id}', [TasksController::class, 'show']);
-//Route::post('/tasks', [TasksController::class, 'store']);
-//Route::put('/tasks/{id}', [TasksController::class, 'update']);
-//Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
